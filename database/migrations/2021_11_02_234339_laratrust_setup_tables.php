5aa2133579e00c4cf81661e942e41606ba173320
grubaz <you@example.com>
2021-11-17 19:12:21 +0100

    db config

--- database/migrations/2021_11_02_234339_laratrust_setup_tables.php
+++ database/migrations/2021_11_02_234339_laratrust_setup_tables.php
@@ -33,8 +33,8 @@ class LaratrustSetupTables extends Migration
 
         // Create table for associating roles to users and teams (Many To Many Polymorphic)
         Schema::create('role_user', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id');
-            $table->unsignedBigInteger('user_id');
+            $table->integer('role_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->string('user_type');
 
             $table->foreign('role_id')->references('id')->on('roles')
